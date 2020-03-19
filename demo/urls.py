94190e097237595151d58c229a9132d828c034ea
nigmasilmi <nigmasilmi.web@gmail.com>
2020-03-19 12:10:10 -0300

    refactored view for Retrieve, Update and Destroy with the class RetrieveUpdateDestroyAPI

--- demo/urls.py
+++ demo/urls.py
@@ -2,13 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from store.api_views import ProductList, ProductCreate, ProductDestroy
+from store.api_views import ProductList, ProductCreate, RetrieveUpdateDestroyProduct
 import store.views
 
 urlpatterns = [
     path('api/v1/products/', ProductList.as_view()),
     path('api/v1/products/create', ProductCreate.as_view()),
-    path('api/v1/products/<int:id>/destroy', ProductDestroy.as_view()),
+    path('api/v1/products/<int:id>/reupde',
+         RetrieveUpdateDestroyProduct.as_view()),
 
     path('admin/', admin.site.urls),
     path('products/<int:id>/', store.views.show, name='show-product'),
